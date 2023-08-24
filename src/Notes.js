68c80a5c3a6e5d928610f16b36f362e1cf1333d2
Adam Burski <burskiadamwork@gmail.com>
2023-08-24 21:22:04 +0200

    Stworzenie komponentu Notes który będzie przetrzymywał notatki

---
+++ src/Notes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Table } from "react-bootsrap";
+
+const header = <h3>List of notes</h3>;
+
+function Notes() {
+    return (
+        <div>
+            {header}
+        </div>
+    )
+}
+export default Notes;
