a24127d880415904be8ef037b79432739ec96969
Adam Burski <burskiadamwork@gmail.com>
2023-08-26 17:18:47 +0200

    Dodanie pojedynczego elementu Note

---
+++ src/Note.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Note = props => {
+    return(
+        <tr>
+            <td>{props.title}</td>
+            <td>{props.category}</td>
+            <td>{props.content}</td>
+            <td>
+                {props.date.getDate()}.{props.date.getMonth()+1}.
+                {props.date.getFullYear()}
+            </td>
+        </tr>
+    )
+}
+export default Note;
